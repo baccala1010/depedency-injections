c3694492766757d193744fa009ea45098201e8d3
Bakhtiyar Baizulla <231388@astanait.edu.kz>
2024-07-27 00:42:11 +0500

    dependency injections via applicationContext.xml

---
+++ src/main/java/org/notfirst/project/BluesRock.java
@@ -0,0 +1,8 @@
+package org.notfirst.project;
+
+public class BluesRock extends RockMusic{
+    @Override
+    public String getSong() {
+        return "Baby, I'm gonna leave you";
+    }
+}
---
+++ src/main/java/org/notfirst/project/GlamMetal.java
@@ -0,0 +1,8 @@
+package org.notfirst.project;
+
+public class GlamMetal extends RockMusic{
+    @Override
+    public String getSong() {
+        return "Kickstart My Heart";
+    }
+}
